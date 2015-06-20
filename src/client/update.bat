7637349aa8c651a069afa6679c0522c388314287
stulgst <Steven.Stulga@ngc.com>
2015-06-20 19:31:26 -0400

    Better update.bat script
    -we now store local backups of dist in dist_old
    -gitignore updated to not track dist_old

--- src/client/update.bat
+++ src/client/update.bat
@@ -1,3 +1,9 @@
-rmdir /s /q ..\server\dist
+@echo off
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-3 delims=/:/ " %%a in ('time /t') do (set mytime=%%a-%%b-%%c)
+set mytime=%mytime: =% 
+
+mkdir ..\server\dist_old
+move ..\server\dist ..\server\dist_old\dist_%mydate%_%mytime%
+
 grunt
-pause
