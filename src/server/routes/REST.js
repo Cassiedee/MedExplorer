330e2225416c39862fff7585f198ef3a52f0f2a3
stulgst <Steven.Stulga@ngc.com>
2015-06-21 10:58:43 -0400

    Replaced index.js with REST.js

--- src/server/routes/REST.js
+++ src/server/routes/REST.js
@@ -4,10 +4,11 @@ var router = express.Router();
 /*
  * GET REST calls
  */
-router.get('/REST/hello/:name', function(req, res) {
+router.get('/REST/hello/:name', hello);
+function hello(req, res) {
   res.json({
     "response": "Hello, " + req.params.name
   });
-});
+}
 
 module.exports = router;
