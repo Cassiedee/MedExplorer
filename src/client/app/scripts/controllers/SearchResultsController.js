245d2df7a5b3c4aebd9f5e24004dbfcb47359e7e
stulgst <Steven.Stulga@ngc.com>
2015-06-22 11:59:44 -0400

    Search bar searches on drug labeling

--- src/client/app/scripts/controllers/SearchResultsController.js
+++ src/client/app/scripts/controllers/SearchResultsController.js
@@ -4,8 +4,12 @@ angular.module('MedExplorer')
   .controller('SearchResultsController', ['$scope', '$http','$state', function($scope, $http, $state) {
     $scope.results = [];
     $scope.searchTerm = $state.params.value;
-    $http.get('/REST/search?datasource=drug&type=event&field=patient.drug.openfda.generic_name&value='+$state.params.value).success(function(data) {
+    $http.get('/REST/search?datasource=drug&type=label&field=openfda.brand_name&value='+$state.params.value).success(function(data) {
       if(!data.error)
         $scope.results = data.response.results;
+      $scope.resultsLength = 0;
+      if($scope.results && $scope.results.length)
+        $scope.resultsLength = $scope.results.length;
+      console.log($scope.resultsLength);
     });
   }]);
