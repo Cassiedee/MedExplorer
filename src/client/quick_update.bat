2650fd694ee5108241a50fed7b10b60b7438072e
stulgst <Steven.Stulga@ngc.com>
2015-06-22 09:50:01 -0400

    Continuing the quick_update.bat re-runs it

--- src/client/quick_update.bat
+++ src/client/quick_update.bat
@@ -1,4 +1,5 @@
 @echo off
+:loop
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
 For /f "tokens=1-3 delims=/:/ " %%a in ('time /t') do (set mytime=%%a-%%b-%%c)
 set mytime=%mytime: =% 
@@ -10,3 +11,4 @@ xcopy /Y /s app ..\server\dist
 xcopy /Y bower_components ..\server
 
 pause
+goto loop
