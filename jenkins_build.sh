cf9987251f2459fd7b4618f52eef953e82a243bc
hallest <Steven.Halle@ngc.com>
2015-06-22 21:54:40 -0400

    ___

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -4,19 +4,19 @@ DIR=$PWD
 
 # Set up server node modules
 echo Setting up server node modules
-cd $DIR\src\server
+cd $DIR/src/server
 npm install
 
-# Build front-end (src\client)
+# Build front-end (src/client)
 echo building front-end
-cd $DIR\src\client
+cd $DIR/src/client
 npm install
 bower install
-cd $DIR\src\client
-.\jenkins_update.bat
+cd $DIR/src/client
+./jenkins_update.bat
 
-# run tests for back-end (src\server)
+# run tests for back-end (src/server)
 echo running back-end tests
-cd $DIR\src\server
+cd $DIR/src/server
 npm install unit.js
-mocha tests\test.js
+mocha tests/test.js
