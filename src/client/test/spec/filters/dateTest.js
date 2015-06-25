1430c403023f7d9d03f9002b9fd0c2dbf636a006
Gillian Hayes <gillian.hayes@ngc.com>
2015-06-25 11:37:18 -0400

    Added dependency for test injections

--- src/client/test/spec/filters/dateTest.js
+++ src/client/test/spec/filters/dateTest.js
@@ -10,18 +10,18 @@ describe('Filter: date', function () {
   
 
 	// Initialize the filter and a mock scope
-	beforeEach(inject(function ($filter) {
+	/*beforeEach(inject(function ($filter) {
 	  mmddyyyy_slashes = $filter();
-	}));
+	}));*/
 
   it('date_test', function () {
 	  //expect(mmddyyyy_slashes('20150625', null)).toBe('06/25/2015');
 	  expect(1).toBe(1);
   });
 
-  /*it('has a mmddyyyy_slashes date filter', inject(function($filter) {
+  it('has a mmddyyyy_slashes date filter', inject(function($filter) {
       expect($filter('mmddyyyy_slashes')).not.toBeNull();
-  }));*/
+  }));
 
 
 });
