d84c0b4d92635e122d401273a65a54087bf69c82
Gillian Hayes <gillian.hayes@ngc.com>
2015-06-26 09:27:11 -0400

    Pushing

--- src/client/test/karma.conf.js
+++ src/client/test/karma.conf.js
@@ -2,7 +2,6 @@
 // http://karma-runner.github.io/0.12/config/configuration-file.html
 // Generated on 2015-06-20 using
 // generator-karma 1.0.0
-
 module.exports = function(config) {
   'use strict';
 
