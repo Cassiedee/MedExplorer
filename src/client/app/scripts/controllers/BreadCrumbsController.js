5b7bde5522df9aed8740b1addab105b9b6ed6b94
dstacy <dan.stacy@ngc.com>
2015-06-26 13:50:45 -0400

    Changed selected tab color

--- src/client/app/scripts/controllers/BreadCrumbsController.js
+++ src/client/app/scripts/controllers/BreadCrumbsController.js
@@ -3,7 +3,6 @@
 
 angular.module('MedExplorer')
 .controller('BreadCrumbsController', ['$scope', '$stateParams', function($scope, $stateParams) {	
-	console.log('In breadcrumbs contoller');
 	if($stateParams.value)
 		$scope.value = $stateParams.value;
 	if($stateParams.source)
@@ -16,10 +15,4 @@ angular.module('MedExplorer')
 		$scope.limit = $stateParams.limit;
 	if($stateParams.tabName)
 		$scope.tabName = $stateParams.tabName;
-	console.log('value ' + $scope.value);
-	console.log('source ' + $scope.source);
-	console.log('type ' + $scope.type);
-	console.log('field ' + $scope.field);
-	console.log('limit ' + $scope.limit);
-	console.log('tabName ' + $scope.tabName);
 }]);
