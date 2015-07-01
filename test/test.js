9cf62271c0ab8c1e2dfcfde551ae5aaf5bd14e01
hallest <Steven.Halle@ngc.com>
2015-07-01 10:52:16 -0400

    Added jenkins base image

--- test/test.js
+++
@@ -1,9 +0,0 @@
-var test = require('unit.js');
-
-//pointless test (you can delete)
-//test.should('foobar').be.equal('foobar');
-describe('Learning by the example', function(){
-  it('example variable', function(){
-    test.assert(true);
-  });
-});
